c3421e1b792ab370fecf31282ebbaccce00f01ab
canturkmen <turkmenc098@gmail.com>
2022-01-28 09:59:41 +0300

    Handling the POST requests.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) =>{
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -8,6 +9,14 @@ app.use((req, res, next) =>{
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE OPTIONS,");
   next();
 })
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body();
+  console.log(post);
+  res.status(201).json({
+    message: "Post has been added."
+  })
+});
 
 app.use('/api/posts',(req, res, next) => {
   const posts = [
