89e16dce526e3f32eb5fd382f0bde4eb25bc3098
canturkmen <turkmenc098@gmail.com>
2022-02-04 20:13:39 +0300

    Added paginator.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
 import {Subscription} from 'rxjs'
 
 import { Post } from '../post.model';
@@ -13,6 +14,9 @@ import { PostService } from "../posts.service";
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postPerPage = 2;
+  pageSizeOptions = [1, 2, 5, 10];
 
   private postsSub: Subscription;
 
@@ -26,6 +30,10 @@ export class PostListComponent implements OnInit, OnDestroy {
     .subscribe((posts: Post[]) => {this.posts = posts
       this.isLoading = false;
     });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   ngOnDestroy(): void {
